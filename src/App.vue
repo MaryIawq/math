633dfe7cb76c6b92467218de6b28d7e61d7817fb
MaryIawq <mary.iawq@gmail.com>
2024-03-06 15:02:12 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header class='flex items-center gap-4'>
+    <img src="./assets/logo.png" alt="logo" />
+    <h1 class='text-stone-900 text-4xl font-bold'>Math()</h1>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
