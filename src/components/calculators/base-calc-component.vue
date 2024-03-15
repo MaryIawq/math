57840c31b29308728004176b041ed5d3bf7d6bcf
MaryIawq <mary.iawq@gmail.com>
2024-03-15 18:29:42 +0900

    feat: base calc functional

--- src/components/calculators/base-calc-component.vue
+++ src/components/calculators/base-calc-component.vue
@@ -3,11 +3,13 @@
     <h2 class='bg-orange-400 font-bold w-full py-2 dark:bg-teal-600 rounded-t-xl'>base</h2>
 
     <div class='bg-white mb-4 dark:bg-neutral-600 relative w-full sm:h-48 h-36 flex flex-col justify-center items-end'>
-      <p class='sm:text-7xl text-5xl font-thin mr-4'>32443,43</p>
-      <input class='bg-neutral-300 dark:bg-neutral-800 sm:text-2xl text-lg text-right sm:p-1 pr-1 font-thin w-full absolute bottom-0'>
+      <p class='sm:text-6xl text-4xl max-w-full font-thin mr-4'>{{ displayedResult }}</p>
+      <input
+        v-model='calcText'
+        class='bg-neutral-300 dark:bg-neutral-800 sm:text-2xl text-lg text-right sm:p-1 pr-1 font-thin w-full absolute bottom-0'>
     </div>
     <div class='flex flex-wrap-reverse items-center justify-center'>
-      <button v-for='calcButton in calcButtons' :key='calcButton' :class='buttonClass(calcButton)'>
+      <button  @click="handleButtonClick(calcButton)" v-for='calcButton in calcButtons' :key='calcButton' :class='buttonClass(calcButton)'>
         {{ calcButton }}
       </button>
     </div>
@@ -15,10 +17,11 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const calcButtons = ref(['(', 0, ')', '=', 1, 2, 3, '-', 4, 5, 6, '+', 7, 8, 9, '/', 'C', '<', '.', '*'])
-
+const calcText = ref('')
+const result = ref('')
 const buttonClass = (button) => {
   if (button === '(' || button === ')') {
     return 'basis-1/5 bg-yellow-400 dark:bg-amber-500 shadow-lg font-thin sm:text-5xl text-3xl sm:py-4 py-2 sm:px-8 px-4 sm:rounded-3xl rounded-lg m-1'
@@ -27,6 +30,34 @@ const buttonClass = (button) => {
   }
   return 'basis-1/5 bg-teal-400 dark:bg-amber-700 shadow-lg font-thin sm:text-5xl text-3xl sm:py-4 py-2 sm:px-8 px-4 sm:rounded-3xl rounded-lg m-1'
 }
+const handleButtonClick = (button) => {
+  if (button === 'C') {
+    calcText.value = '';
+    result.value = '';
+  } else if (button === '<') {
+    calcText.value = calcText.value.slice(0, -1);
+  } else if (button === '=') {
+    try {
+      let evalResult = eval(calcText.value);
+      result.value = evalResult.toString().substring(0, 13);
+      if (evalResult.toString().length > 13) {
+        result.value += '...';
+      }
+    } catch (error) {
+      result.value = 'Error';
+    }
+  } else {
+    calcText.value += button;
+  }
+}
+
+const displayedResult = computed(() => {
+  if (result.value.length > 14) {
+    return result.value.substring(0, 13) + '...';
+  } else {
+    return result.value;
+  }
+});
 </script>
 
 <style scoped>
